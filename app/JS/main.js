b01fa035f6b47d94a9d4eaafd4837d209b3429c5
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-10-30 12:29:40 -0400

    Z

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,1 +1,18 @@
 import "../CSS/style.css";
+import "../CSS/variable.css";
+import { motleyCrueSongs } from "./songs.js";
+
+const songContainer = document.getElementById("song-container");
+
+motleyCrueSongs.forEach((song) => {
+  const card = `
+    <div class="card card-ani">
+      <h2>${song.name}</h2>
+      <img class="images" src="${song.imageUrl}" alt="${song.altText}" />
+      <h3>Album: ${song.album}</h3>
+      <p>Release Year: ${song.releaseYear}</p>
+      <p>Times Played: ${song.timesPlayed}</p>
+    </div>
+  `;
+  songContainer.innerHTML += card;
+});
