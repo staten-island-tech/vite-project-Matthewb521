13d572f5888a51699be1bb3caaa9180808b6e075
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-11-09 10:10:29 -0500

    a

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,6 +1,10 @@
 import "../CSS/style.css";
 import "../CSS/variable.css";
 import { motleyCrueSongs } from "./songs.js";
+export { lowToHigh, highToLow, displaySongs };
+
+const songContainer = document.getElementById("song-container");
+let displayedSongs = [...motleyCrueSongs];
 
 function createSongCard(song) {
   return `
@@ -14,31 +18,113 @@ function createSongCard(song) {
     `;
 }
 
-function displaySorted(songs) {
-  songContainer.innerHTML = songs.map(createSongCard).join("");
+function displaySongs(albumFilter) {
+  displayedSongs = motleyCrueSongs.filter(
+    (song) => !albumFilter || song.album === albumFilter
+  );
+  songContainer.innerHTML = displayedSongs.map(createSongCard).join("");
+}
+
+function lowToHigh() {
+  const sortedSongs = [...displayedSongs].sort(
+    (a, b) => a.timesPlayed - b.timesPlayed
+  );
+  songContainer.innerHTML = sortedSongs.map(createSongCard).join("");
+}
+
+function highToLow() {
+  const sortedSongs = [...displayedSongs].sort(
+    (a, b) => b.timesPlayed - a.timesPlayed
+  );
+  songContainer.innerHTML = sortedSongs.map(createSongCard).join("");
 }
 
 document
   .querySelector("#btn-filterLowToHigh")
-  .addEventListener("click", function () {
-    const sortedSongs = [...motleyCrueSongs].sort(
-      (a, b) => a.timesPlayed - b.timesPlayed
-    );
-    displaySorted(sortedSongs);
-  });
+  .addEventListener("click", lowToHigh);
 
 document
   .querySelector("#btn-filterHighToLow")
-  .addEventListener("click", function () {
-    const sortedSongs = [...motleyCrueSongs].sort(
-      (a, b) => b.timesPlayed - a.timesPlayed
-    );
-    displaySorted(sortedSongs);
-  });
+  .addEventListener("click", highToLow);
 
-document
-  .querySelector("#btnclearFilters")
-  .addEventListener("click", function () {
-    displaySorted();
-  });
+displaySongs();
 
+document.querySelector("#btn-0").addEventListener("click", function () {
+  const classesToRemove = [
+    "dr_feelgood",
+    "dirt_soundtrack",
+    "shout_devil",
+    "too_fast",
+  ];
+
+  if (classesToRemove.some((cls) => document.body.classList.contains(cls))) {
+    document.body.classList.add("all_songs");
+    document.body.classList.remove(...classesToRemove);
+
+    displaySongs();
+  }
+});
+
+document.querySelector("#btn-1").addEventListener("click", function () {
+  const classesToRemove = [
+    "all_songs",
+    "dirt_soundtrack",
+    "shout_devil",
+    "too_fast",
+  ];
+
+  if (classesToRemove.some((cls) => document.body.classList.contains(cls))) {
+    document.body.classList.add("dr_feelgood");
+    document.body.classList.remove(...classesToRemove);
+
+    displaySongs("Dr. Feelgood");
+  }
+});
+
+document.querySelector("#btn-2").addEventListener("click", function () {
+  const classesToRemove = [
+    "all_songs",
+    "dr_feelgood",
+    "shout_devil",
+    "too_fast",
+  ];
+
+  if (classesToRemove.some((cls) => document.body.classList.contains(cls))) {
+    document.body.classList.add("dirt_soundtrack");
+    document.body.classList.remove(...classesToRemove);
+
+    displaySongs("The Dirt Soundtrack");
+  }
+});
+
+document.querySelector("#btn-3").addEventListener("click", function () {
+  const classesToRemove = [
+    "all_songs",
+    "dirt_soundtrack",
+    "dr_feelgood",
+    "too_fast",
+  ];
+
+  if (classesToRemove.some((cls) => document.body.classList.contains(cls))) {
+    document.body.classList.add("shout_devil");
+    document.body.classList.remove(...classesToRemove);
+
+    displaySongs("Shout at the Devil");
+  }
+});
+
+document.querySelector("#btn-4").addEventListener("click", function () {
+  const classesToRemove = [
+    "all_songs",
+    "dirt_soundtrack",
+    "shout_devil",
+    "dr_feelgood",
+  ];
+
+  if (classesToRemove.some((cls) => document.body.classList.contains(cls))) {
+    document.body.classList.add("too_fast");
+    document.body.classList.remove(...classesToRemove);
+
+    displaySongs("Too Fast for Love");
+  }
+});
